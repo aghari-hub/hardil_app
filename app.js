7c6073cd67adafcb6172c9d13de8692c03cf16b9
hari <aghari128@gmail.com>
2026-01-29 16:38:16 +0530

    modification in animation and web deployment code

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-function go(page) {
-  document.body.style.opacity = 0;
-  setTimeout(() => {
-    window.location.href = page;
-  }, 200);
-}
-
-function open(url) {
-  window.open(url, "_self");
-}
+function open(url){window.location.href=url;}
